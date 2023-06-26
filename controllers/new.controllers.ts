f9ecb86fc0340e057520a5cf1f4ca8e6b17330f8
goku-ts <hgb.mega@gmail.com>
2023-06-26 08:00:42 +0000

    added try catch

--- controllers/new.controllers.ts
+++ controllers/new.controllers.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 
 export const newController = (req: Request, res: Response) => {
-    res.json({ message: "new route created" })
+    try {
+        res.json({ message: "new route created" })
+
+    } catch (error) {
+
+    }
 }
